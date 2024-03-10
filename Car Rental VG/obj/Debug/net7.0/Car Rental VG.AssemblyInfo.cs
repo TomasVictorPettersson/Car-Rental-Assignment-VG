9dae2e1197a6882ae9db92513ee0e1f2b945f667
Tomas <tom_pa_15@hotmail.com>
2024-03-10 22:27:50 +0100

    code

--- Car Rental VG/obj/Debug/net7.0/Car Rental VG.AssemblyInfo.cs
+++ Car Rental VG/obj/Debug/net7.0/Car Rental VG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car Rental VG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb38bef0b06c9230fe2a29411857fe8b9932f5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car Rental VG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car Rental VG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Car Rental.Common/obj/Debug/net7.0/Car Rental.Common.AssemblyInfo.cs
+++ Car Rental.Common/obj/Debug/net7.0/Car Rental.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car Rental.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb38bef0b06c9230fe2a29411857fe8b9932f5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car Rental.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car Rental.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
